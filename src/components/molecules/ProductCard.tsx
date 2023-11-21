9e3afa6ebd31884517f65e9fb0a2b43df78b5b54
kinakomoch7 <book-insect14@outlook.jp>
2023-11-21 22:23:50 +0900

    [add]product実装

--- src/components/molecules/ProductCard.tsx
+++ src/components/molecules/ProductCard.tsx
@@ -1,21 +1,22 @@
 import { Grid, Card, CardMedia, CardContent } from "@mui/material";
-import icon from "../../common/images/icon.png";
 import { Typo } from "../atomos/Typo";
+import { cardHeadingStyle } from "../../common/styles/heading";
 
 type Props = {
   message: string;
   cardText: string;
+  png: string;
 };
 
 export const ProductCard = (props: Props) => {
-  const { message, cardText } = props;
+  const { message, cardText, png } = props;
 
   return (
     <>
       <Grid item xs={12} md={6}>
         <Card>
-          <Typo children={message} align="center"/>
-          <CardMedia style={{ height: 0, paddingTop: "56.25%" }} image={icon} />
+          <Typo children={message} align="center" {...cardHeadingStyle}/>
+          <CardMedia style={{ height: 0, paddingTop: "56.25%" }} image={png} />
           <CardContent><Typo children={cardText} /></CardContent>
         </Card>
       </Grid>
--- src/components/organisms/Production.tsx
+++ src/components/organisms/Production.tsx
@@ -1,23 +1,24 @@
-import { Card, Container, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 import { ProductCard } from "../molecules/ProductCard";
 import { Typo } from "../atomos/Typo";
 import { headingStyle } from "../../common/styles/heading";
+import portFolioPng from '../../common/images/products/portFolio.png'
+import publicFundPng from '../../common/images/products/publicFund.png'
 
 export const Production = () => {
   return (
     <>
-      <Container>
-        <Card>
-          <Typo children='Production' {...headingStyle}/>
-          <Grid container spacing={{ xs: 1, md: 2 }}>
-            <ProductCard message="todoApp" cardText="これはtodoアプリです" />
-            <ProductCard
-              message="dicord-clone-app"
-              cardText="これはdicordのクローンです"
-            />
-          </Grid>
-        </Card>
-      </Container>
+      <Typo children='Production' {...headingStyle}/>
+      <Grid container spacing='8'>
+        <ProductCard
+          message="port-folio-app"
+          cardText="自分のポートフォリオのアプリです。フレキシブルデザインに対応しています"
+          png={ portFolioPng } />
+        <ProductCard
+          message="公金支出可視化app"
+          cardText="TOKYO OPEN DATAにある東京の公金支出を可視化したアプリです。"
+        png={ publicFundPng }/>
+      </Grid>
     </>
   );
 };
